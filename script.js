8092e355c7ad00a307da9c6395df8f3c9677310e
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2023-12-15 10:04:42 +0000

    Create card values

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-class Deck {
-    constructor(cards);{
-        this.cards = cards
-    }
+/*Card values*/
+const values = ['1', '2', '3', '4', '5', '6', '7', '8', '1' ,'2' ,'3', '4', '5', '6', '7', '8'];
 
-}
-class Card {
-    
-}
+
