75a07b3ad74ba841f7395d2f6fc650f1d96eb485
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2023-12-15 11:05:00 +0000

    Add initial style for the whole page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 /*Card values*/
 const values = ['1', '2', '3', '4', '5', '6', '7', '8', '1' ,'2' ,'3', '4', '5', '6', '7', '8'];
 
-
+/*Deal Cards*/
+var deal = function(){
+    var card = Math.random()
+return values;}
