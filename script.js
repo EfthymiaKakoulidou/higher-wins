f63cf54631e895181d1d05303a8a2b4b2bd63abe
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2023-12-15 11:22:36 +0000

    Add comments to js with all the functions

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 const values = ['1', '2', '3', '4', '5', '6', '7', '8', '1' ,'2' ,'3', '4', '5', '6', '7', '8'];
 
 /*Deal Cards*/
-var deal = function(){
-    var card = Math.random()
+const deal = function(){
+    const card = Math.random()
 return values;}
+
+/*Player and computer choose card*/
+
+/*Comparison of cards chosen*/
+
+/*Update score*/
+
+/*Cards chosen deactivated*/
+
+/*Repeat until all cards deactivated*/
